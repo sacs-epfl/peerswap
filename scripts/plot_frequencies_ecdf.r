9b616a4c9773c08f0492925f7fd9aa942f3d2a24
Martijn de Vos <martijn.devos@epfl.ch>
2024-01-09 15:56:25 +0100

    Updated scripts to also include node sampling

--- scripts/plot_frequencies_ecdf.r
+++ scripts/plot_frequencies_ecdf.r
@@ -2,14 +2,21 @@ library(ggplot2)
 library(dplyr)
 
 # Frequencies per node
-dat <- read.csv("data/n_50_k_4_t_1/frequencies.csv")
+dat <- read.csv("data/n_1000_k_7_t_7/frequencies.csv")
+synthetic_file_path <- "data/n_1000_k_7_t_7/frequencies_synthetic.csv"
+if (file.exists(synthetic_file_path)) {
+  dat_synthetic <- read.csv(synthetic_file_path)
+  dat <- rbind(dat, dat_synthetic)
+}
 dat <- dat[dat$freq > 0,]
+dat <- dat %>% mutate(group = paste(algorithm, " (n=", nodes, ",k=", k, ")", sep=""))
 
-p <- ggplot(dat, aes(x=freq)) +
+p <- ggplot(dat, aes(x=freq, group=group, color=group, linetype=group)) +
      stat_ecdf() +
      theme_bw() +
-     xlab("Frequency") +
-     ylab("ECDF")
+     xlab("Node Frequency") +
+     ylab("ECDF") +
+     theme(legend.position=c(0.75, 0.3), legend.box.background = element_rect(colour = "black"))
 
 ggsave("data/node_frequencies_ecdf.pdf", p, width=5, height=3)
 
--- scripts/synthetically_sample_nbhs.py
+++ scripts/synthetically_sample_nbhs.py
@@ -2,22 +2,31 @@ import os
 import random
 from collections import defaultdict
 
-N = 50
-K = 4
-T = 1
-RUNS = 50000000
+N = 1000
+K = 7
+T = 7
+RUNS = 50000
 
-freqs = defaultdict(lambda: 0)
+nbh_freqs = defaultdict(lambda: 0)
+node_freqs = defaultdict(lambda: 0)
+
 for run in range(RUNS):
     if run % 1000000 == 0:
         print("Generated %d neighbourhoods..." % run)
 
     nbh = tuple(sorted(random.sample(range(0, N - 1), K)))
-    freqs[nbh] += 1
+    nbh_freqs[nbh] += 1
+    for peer in nbh:
+        node_freqs[peer] += 1
 
 # Write away the frequencies
 with open(os.path.join("data", "n_%d_k_%d_t_%g" % (N, K, T), "nbh_frequencies_synthetic.csv"), "w") as out_file:
     out_file.write("algorithm,nodes,k,time_per_run,nbh,freq\n")
-    for nbh, freq in freqs.items():
+    for nbh, freq in nbh_freqs.items():
         nbh_str = "-".join(["%d" % peer for peer in nbh])
         out_file.write("synthetic,%d,%d,%g,%s,%d\n" % (N, K, T, nbh_str, freq))
+
+with open(os.path.join("data", "n_%d_k_%d_t_%g" % (N, K, T), "frequencies_synthetic.csv"), "w") as out_file:
+    out_file.write("algorithm,nodes,k,time_per_run,node,freq\n")
+    for node, freq in node_freqs.items():
+        out_file.write("synthetic,%d,%d,%g,%d,%d\n" % (N, K, T, node, freq))
